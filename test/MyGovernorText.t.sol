4478becac5623de3821c92a22cd16e09db76ca45
Abhinav Sharma <abhi5237@gmail.com>
2024-03-14 19:53:25 +0530

    test update

--- test/MyGovernorText.t.sol
+++ test/MyGovernorText.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {MyGovernor} from "../src/MyGovernor.sol";
 import {Box} from "../src/Box.sol";
 import {TimeLock} from "../src/TimeLock.sol";
@@ -19,7 +19,13 @@ contract MyGovernorTest is Test {
     address[] proposers;
     address[] executors;
 
+    uint256[] values;
+    bytes[] calldatas;
+    address[] targets;
+
     uint256 public constant MIN_DELAY = 3600; // 1 hour - after a vote passes
+    uint256 public constant VOTING_DELAY = 1; // How many blocks till a vote is active
+    uint256 public constant VOTING_PERIOD = 50400; // Voting period
 
     function setUp() public {
         govToken = new GovToken();
@@ -47,5 +53,57 @@ contract MyGovernorTest is Test {
     function testCant_update_Box_without_Governance() public {
         vm.expectRevert();
         box.store(1);
+    }
+
+    function testGovernance_Updates_Box() public {
+        uint256 valueToStore = 888;
+        string memory description = "Store 1 in Box";
+        bytes memory encodedFunctionCall = abi.encodeWithSignature(
+            "store(uint256)",
+            valueToStore
+        );
+
+        values.push(0);
+        calldatas.push(encodedFunctionCall);
+        targets.push(address(box));
+
+        // 1. Propose to the DAO
+        uint256 proposalId = governor.propose(
+            targets,
+            values,
+            calldatas,
+            description
+        );
+
+        // View the state
+        console.log("Proposal State", uint256(governor.state(proposalId)));
+
+        vm.warp(block.timestamp + VOTING_DELAY + 1);
+        vm.roll(block.number + VOTING_DELAY + 1);
+
+        // 2. Vote!
+        string memory reason = "cuz blue frog is cool";
+
+        uint8 voteWay = 1; //Voting yes, its enum in GovernorCountingSimple.sol in openzeppelin
+        vm.prank(USER);
+        governor.castVoteWithReason(proposalId, voteWay, reason);
+
+        // Now lets speedup the voting period since I am the only voter
+        vm.warp(block.timestamp + VOTING_PERIOD + 1);
+        vm.roll(block.number + VOTING_PERIOD + 1);
+
+        // 3. Quere the Trx
+        // Looking in the openzeppelin queue function
+        bytes32 descriptionHash = keccak256(abi.encodePacked(description));
+        governor.queue(targets, values, calldatas, descriptionHash);
+
+        vm.warp(block.timestamp + MIN_DELAY + 1);
+        vm.roll(block.number + MIN_DELAY + 1);
+
+        // 4. Execute
+        governor.execute(targets, values, calldatas, descriptionHash);
+
+        console.log("Box value:", box.retrieve());
+        assertEq(box.retrieve(), valueToStore);
     }
 }
